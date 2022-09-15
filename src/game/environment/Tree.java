bec69ea9e16f54b5cf8a20adf942d75ffadf0c81
Chongjie <cche0204@student.monash.edu>
2022-09-15 22:16:10 +1000

    implemented day and night effects

--- src/game/environment/Tree.java
+++ src/game/environment/Tree.java
@@ -2,8 +2,9 @@ package game.environment;
 
 import edu.monash.fit2099.engine.positions.Ground;
 import game.Element;
+import game.time.TimePerception;
 
-public class Tree extends SpawningGround {
+public class Tree extends SpawningGround implements TimePerception {
 
     /**
      * Constructor.
@@ -12,5 +13,15 @@ public class Tree extends SpawningGround {
     public Tree() {
         super('T',15,15);
         this.addCapability(Element.GRASS);
+    }
+
+    @Override
+    public void dayEffect() {
+
+    }
+
+    @Override
+    public void nightEffect() {
+
     }
 }
