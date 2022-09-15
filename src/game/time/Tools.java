f90ca434ad67a2e2f78afbdc01c4a7cf687fa078
Chongjie <cche0204@student.monash.edu>
2022-09-15 22:14:13 +1000

    added a class called tools which simulates a chance action and returns a boolean

---
+++ src/game/time/Tools.java
@@ -0,0 +1,11 @@
+package game.time;
+import java.util.Random;
+public class Tools {
+    public Tools() {
+    }
+    public static boolean chanceSimulation(int chance){
+        Random rand = new Random();
+        return rand.nextInt(100) <= 10;
+
+    }
+}
