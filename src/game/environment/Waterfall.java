7667377aa8ff48726fcc10fa58b002236e20dd12
Chongjie <cche0204@student.monash.edu>
2022-09-17 18:05:03 +1000

    deleted some imports that weren't being used

--- src/game/environment/Waterfall.java
+++ src/game/environment/Waterfall.java
@@ -1,7 +1,13 @@
 package game.environment;
 
+import edu.monash.fit2099.engine.items.DropItemAction;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 import game.Element;
+import game.Spawn;
+import game.items.Pokefruit;
+import game.pokemons.Charmander;
+import game.time.Tools;
 
 public class Waterfall extends SpawningGround {
 
@@ -12,5 +18,18 @@ public class Waterfall extends SpawningGround {
     public Waterfall() {
         super('W',20,20);
         this.addCapability(Element.WATER);
+    }
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        if (Tools.chanceSimulation(20)) {
+            this.addCapability(Spawn.SQUIRTLE);
+        }
+        if (Tools.chanceSimulation(20)) {
+            Pokefruit pokefruit= new Pokefruit(Element.WATER);
+            new DropItemAction(pokefruit);
+        }
+
     }
 }
